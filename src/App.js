ca5737fcfb757382fda7d6ed9a38c5ee65584f4b
AMAN SAHARAN <aman@AMANs-MacBook-Pro.local>
2019-09-10 19:48:24 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import HomePage from './pages/homepage.component';
 
+import { Route } from 'react-router-dom';
+
 import './App.css';
+
+const HatsPage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page is here...</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
+import { withRouter } from 'react-router-dom';
+
 const MenuItem = props => (
-  <div className={`${props.size} menu-item`}>
+  <div
+    className={`${props.size} menu-item`}
+    onClick={() => props.history.push(`${props.match.url}${props.linkUrl}`)}
+  >
     <div
       style={{ backgroundImage: `url(${props.imageUrl})` }}
       className="background-image"
@@ -14,4 +19,4 @@ const MenuItem = props => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
