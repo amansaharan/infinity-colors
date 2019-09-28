6b7c56d5f778e37f04ba3f5aaff887f04f98bf91
AMAN SAHARAN <aman@AMANs-MacBook-Pro.local>
2019-09-28 18:58:07 +0530

    added cart-icon and cart-dropdown to our app also added cart-reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items"></div>
+      <button>BUY</button>
+    </div>
+  );
+};
+
+export default CartDropdown;
